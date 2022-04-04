22cb4db9115297b34d5eca67777bc4a7922c5e98
senthil <asen.nathan@gmail.com>
2022-04-04 21:04:35 +0530

    Initial commit

---
+++ RWPickFlavor/AuthSDK/AuthSDK.swift
@@ -0,0 +1,16 @@
+//
+//  AuthSDK.swift
+//  RWPickFlavor
+//
+//  Created by Senthilnathan on 4/4/22.
+//
+
+import Foundation
+
+class AuthSDK {
+    
+    func Authenticate() {
+        
+    }
+    
+}
